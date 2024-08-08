ffa001c84bba4ffd9302edfceff126488f3b4a12
pradeepGreen00 <dev2@greensturn.com>
2024-08-08 17:26:05 +0530

    change origin

--- server.js
+++ server.js
@@ -1,37 +1,26 @@
 require("dotenv").config();
 
-const app = require('express')();
-
+const express = require('express');
+const app = express();
 const bp = require('body-parser');
-
-const cors = require("cors");
+const cors = require('cors');
 
 const port = process.env.AWS_MS_PORT || 3003;
 
-app.use(bp.json({limit:'100mb'}));
+// CORS middleware to allow all origins
+app.use(cors({
+  origin: '*', // Allow all origins
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS','PATCH'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
+
+app.use(bp.json({ limit: '100mb' }));
+app.use(bp.urlencoded({ extended: true, limit: '100mb' }));
+
+// Routes
 app.use('/v1', require("./router/s3.js"));
 
-app.use(bp.urlencoded({
-  extended: true,
-  limit: '100mb'
-}));
-
-app.use(cors({
-  origin: '*'
-}));
-
-// app.use(function (req, res, next) {
-//   res.header(
-//     "Access-Control-Allow-Origin",
-//     "*"
-//   );
-//   res.header(
-//     "Access-Control-Allow-Headers",
-//     "Origin, X-Requested-With, Content-Type, Accept, x-access-token"
-//   );
-//   res.header("Access-Control-Allow-Methods", "GET,PUT,POST,OPTIONS");
-//   next();
-// });
+// Error handling
 app.use((error, req, res, next) => {
   if (error instanceof SyntaxError) {
     return res.status(400).send({ status: 400, success: false, message: 'Bad request.', error });
@@ -39,22 +28,20 @@ app.use((error, req, res, next) => {
   next();
 });
 
-// app.use('/v1', require("./routes/route"));
-
+// Root route
 app.get("/", (req, res) => {
-  res.send({ msg: `aws Server is running fine ${port}` });
+  res.send({ msg: `AWS Server is running fine on port ${port}` });
 });
 
+// Health check route
 app.get("/health", (req, res) => {
   try {
-    return res.status(200).send({ status: true, message: `AWS CI--CD is running fine at ${port}`});
-
+    return res.status(200).send({ status: true, message: `AWS CI--CD is running fine at ${port}` });
   } catch (error) {
     return res.status(500).send({ status: false, message: `AWS CI--CD down right now at ${port}.` });
   }
 });
 
-
 app.listen(port, async () => {
-  console.log(`aws listening at http://localhost:${port}`);
+  console.log(`AWS listening at http://localhost:${port}`);
 });
